d0078a976c2254ce3038ffee624c6a93f94d22cd
souyang <souyang@opentext.com>
2021-08-13 15:14:31 -0400

    create typescript support for wasm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const start = () => {
+  (() => {
     import("collaborative-text-editor").then((wasm) => {
       wasm.greet();
       wasm.start_websocket();
       console.log("All modules loaded");
     });
-  };
-
-  start();
+  })();
 
   return <div> React App, typescript with wasm </div>;
 }
