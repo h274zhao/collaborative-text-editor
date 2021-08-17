67afd5fe76f0c9df28ae8573ec695b61589162e5
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2021-08-17 16:59:03 -0400

    added user list to server

--- src/App.tsx
+++ src/App.tsx
@@ -96,15 +96,15 @@ export default function App(props: Props) {
 
   useEffect(() => {
     const chat: any = document.getElementById("chat")
-    const uri: string = 'ws://localhost:8000/';
+    const uri: string = 'ws://localhost:8000/editor';
     const ws = new WebSocket(uri);
 
-    ws.onopen = function () {
+    ws.onopen = function (event) {
       chat.innerHTML = '<em> Connected</em>';
-      ws.send("My name is John");
     };
     ws.onmessage = function (event) {
-      alert(`[message] Data received from server: ${event.data}`);
+      setUserList(JSON.parse(event.data).names)
+      console.log(userList)
     };
     ws.onclose = function () {
       chat.innerHTML = '<em> Disconnected</em>';
