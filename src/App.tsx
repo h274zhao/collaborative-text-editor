efda2f31ecc9382e3d8b024dfca83b18ce270665
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2021-08-18 17:40:42 -0400

    moved back ws

--- src/App.tsx
+++ src/App.tsx
@@ -39,11 +39,10 @@ export default function App() {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [users, setUsers] = useState<Record<number, UserInfo>>({});
   const [editor, setEditor] = useState<editor.IStandaloneCodeEditor>();
-  const [, updateState] = useState({});
   const textEditor = useRef<TextEditor>();
   const id = useHash();
 
-  const forceUpdate = useCallback(() => updateState({}), []);
+  const updateUserList = useCallback((users: any) => setUsers(users), []);
 
   useEffect(() => {
     if (editor?.getModel()) {
@@ -54,65 +53,10 @@ export default function App() {
       textEditor.current = new TextEditor({
         uri: getWsUri(id),
         editor,
-        onChangeUsers: setUsers
+        onChangeUsers: updateUserList
       })
-
-      const ws = new WebSocket(getWsUri(id));
-
-      const curr = textEditor.current
-
-      if (curr) {
-        ws.onopen = () => {
-          curr.connecting = false;
-          curr.ws = ws;
-          curr.sendInfo();
-          ws.send("This is a new connection");
-        };
-        ws.onclose = () => {
-          if (curr.ws) {
-            curr.ws = undefined;
-            if (++curr.recentFailures >= 5) {
-              // If we disconnect 5 times within 15 reconnection intervals, then the
-              // client is likely desynchronized and needs to refresh.
-              curr.dispose();
-              curr.options.onDesynchronized?.();
-            }
-          } else {
-            curr.connecting = false;
-          }
-        };
-        ws.onmessage = ({ data }) => {
-          try {
-            const json = JSON.parse(data);
-            curr.users = {}
-            json.users.forEach((user: string, i: number) => {
-              let userInfo: UserInfo = {
-                name: user
-              };
-              curr.users[i] = userInfo;
-            });
-            setUsers(curr.users);
-            forceUpdate()
-          }
-          catch (e) {
-            if (data === "This is a new connection") {
-              ws.send(curr.model.getValue());
-            }
-            else if (data !== curr.model.getValue()) {
-              curr.model.setValue(data);
-            }
-          }
-          curr.ignoreChanges = false;
-        }
-      }
     }
   }, [editor, setUsers])
-
-
-  useEffect(() => {
-    console.log(users)
-  }, [users, textEditor.current?.users])
-
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -202,11 +146,6 @@ export default function App() {
         <div className="editor">
           <Editor
             onMount={(editor) => setEditor(editor)}
-            onChange={() => {
-              console.log("hiu")
-              if (textEditor.current?.users)
-                setUsers(textEditor.current?.users)
-            }}
           />
         </div>
       </div>
--- src/textEditor.ts
+++ src/textEditor.ts
@@ -16,23 +16,23 @@ export type UserInfo = {
   readonly name: string;
 };
 class TextEditor {
-  public ws?: WebSocket;
-  public recentFailures: number = 0;
-  public connecting?: boolean;
-  public readonly model: editor.ITextModel;
-  public readonly onChangeHandle: IDisposable;
- // public readonly tryConnectId: number;
-  public readonly resetFailuresId: number;
+  private ws?: WebSocket;
+  private recentFailures: number = 0;
+  private connecting?: boolean;
+  private readonly model: editor.ITextModel;
+  private readonly onChangeHandle: IDisposable;
+  private readonly tryConnectId: number;
+  private readonly resetFailuresId: number;
 
 
-  public currentValue: string = "";
-  public ignoreChanges: boolean = false;
+  private currentValue: string = "";
+  private ignoreChanges: boolean = false;
 
-  public me: number = -1;
-  public revision: number = 0;
-  public buffer?: OpSeq;
-  public myInfo?: UserInfo;
-  public users: Record<number, UserInfo> = {};
+  private me: number = -1;
+  private revision: number = 0;
+  private buffer?: OpSeq;
+  private myInfo?: UserInfo;
+  private users: Record<number, UserInfo> = {};
 
   constructor(readonly options: Options) {
     this.model = options.editor.getModel()!;
@@ -40,8 +40,8 @@ class TextEditor {
       this.onChange(e)
     );
     const interval = options.reconnectInterval ?? 1000;
-   // this.tryConnect();
- //   this.tryConnectId = window.setInterval(() => this.tryConnect(), interval);
+    this.tryConnect();
+    this.tryConnectId = window.setInterval(() => this.tryConnect(), interval);
     this.resetFailuresId = window.setInterval(
       () => (this.recentFailures = 0),
       15 * interval
@@ -49,61 +49,62 @@ class TextEditor {
   }
 
   dispose() {
-//    window.clearInterval(this.tryConnectId);
+    window.clearInterval(this.tryConnectId);
     window.clearInterval(this.resetFailuresId);
     this.onChangeHandle.dispose();
     this.ws?.close();
   }
 
-  // private tryConnect() {
-  //   if (this.connecting || this.ws) return;
-  //   this.connecting = true;
-  //   const ws = new WebSocket(this.options.uri);
-  //   ws.onopen = () => {
-  //     this.connecting = false;
-  //     this.ws = ws;
-  //     this.sendInfo();
-  //     ws.send("This is a new connection");
-  //   };
-  //   ws.onclose = () => {
-  //     if (this.ws) {
-  //       this.ws = undefined;
-  //       if (++this.recentFailures >= 5) {
-  //         // If we disconnect 5 times within 15 reconnection intervals, then the
-  //         // client is likely desynchronized and needs to refresh.
-  //         this.dispose();
-  //         this.options.onDesynchronized?.();
-  //       }
-  //     } else {
-  //       this.connecting = false;
-  //     }
-  //   };
-  //   ws.onmessage = ({ data }) => {
-  //     try {
-  //       const json = JSON.parse(data);
-  //       json.users.forEach((user: string, i: number) => {
-  //         let userInfo: UserInfo = {
-  //           name: user
-  //         };
-  //         this.users[i] = userInfo;
-  //       });
-  //       console.log(this.users)
-  //       this.options.onChangeUsers?.(this.users);
-  //     }
-  //     catch (e) {
-  //       if (data === "This is a new connection") {
-  //         ws.send(this.model.getValue());
-  //       }
-  //       else if (data !== this.model.getValue()) {
-  //         this.model.setValue(data);
-  //       }
-  //     }
-  //     this.ignoreChanges = false;
-  //   }
-  // }
+  private tryConnect() {
+    if (this.connecting || this.ws) return;
+    this.connecting = true;
+    const ws = new WebSocket(this.options.uri);
+    ws.onopen = () => {
+      this.connecting = false;
+      this.ws = ws;
+      this.sendInfo();
+      ws.send("This is a new connection");
+    };
+    ws.onclose = () => {
+      if (this.ws) {
+        this.ws = undefined;
+        if (++this.recentFailures >= 5) {
+          // If we disconnect 5 times within 15 reconnection intervals, then the
+          // client is likely desynchronized and needs to refresh.
+          this.dispose();
+          this.options.onDesynchronized?.();
+        }
+      } else {
+        this.connecting = false;
+      }
+    };
+    ws.onmessage = ({ data }) => {
+      try {
+        const json = JSON.parse(data);
+        this.users = {}
+        json.users.forEach((user: string, i: number) => {
+          let userInfo: UserInfo = {
+            name: user
+          };
+          this.users[i] = userInfo;
+        });
+        console.log(this.users)
+        this.options.onChangeUsers?.(this.users);
+      }
+      catch (e) {
+        if (data === "This is a new connection") {
+          ws.send(this.model.getValue());
+        }
+        else if (data !== this.model.getValue()) {
+          this.model.setValue(data);
+        }
+      }
+      this.ignoreChanges = false;
+    }
+  }
 
 
-  public onChange(event: editor.IModelContentChangedEvent) {
+  private onChange(event: editor.IModelContentChangedEvent) {
     if (!this.ignoreChanges) {
       this.ignoreChanges = true;
       console.log(event.changes);
@@ -112,7 +113,7 @@ class TextEditor {
     }
   }
 
-  public sendInfo() {
+  private sendInfo() {
     if (this.myInfo) {
       this.ws?.send(`{"ClientInfo":${JSON.stringify(this.myInfo)}}`);
     }
