aaf57a89ee0dc3d264baf5a498b065190695f092
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2021-08-19 18:16:56 -0400

    added rooms

--- src/App.tsx
+++ src/App.tsx
@@ -1,186 +1,20 @@
-import { useEffect, useState, useRef, useCallback } from 'react';
-import Editor from "@monaco-editor/react";
-import { editor } from "monaco-editor/esm/vs/editor/editor.api";
-import TextEditor, { UserInfo } from "./textEditor";
-import useHash from "./useHash"
+import { useState } from 'react';
+import TextEditor from './page/TextEditor';
+import Register from './page/Register';
 import './App.css';
 
-// MATERIAL UI
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import PersonIcon from '@material-ui/icons/Person';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import './App.css';
-
-const drawerWidth = 240;
-
-//TO-DO: need to do change the code to match our own server implementation
-function getWsUri(id: string) {
-  const uri = 'ws://localhost:8000/editor';
-  return uri;
-}
 
 export default function App() {
-  const classes = useStyles();
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const [users, setUsers] = useState<Record<number, UserInfo>>({});
-  const [editor, setEditor] = useState<editor.IStandaloneCodeEditor>();
-  const textEditor = useRef<TextEditor>();
-  const id = useHash();
+  const [inRoom, setInRoom] = useState(false)
+  const [ws, setWs] = useState("")
 
-  const updateUserList = useCallback((users: any) => setUsers(users), []);
+  const toggleInRoom = () => setInRoom(!inRoom);
 
-  useEffect(() => {
-    if (editor?.getModel()) {
-      const model = editor.getModel()!;
-      model.setValue("");
-      model.setEOL(0);
-
-      textEditor.current = new TextEditor({
-        uri: getWsUri(id),
-        editor,
-        onChangeUsers: updateUserList
-      })
-    }
-  }, [editor, setUsers])
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+  let screen = inRoom ? <TextEditor ws={ws}/> : <Register toggleInRoom={toggleInRoom} setWs={setWs} />
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            <div style={{ display: 'flex' }}>
-              OpenText Collaborative Editor
-            </div>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        <Hidden smUp implementation="css">
-          <Drawer
-            variant="temporary"
-            anchor="left"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{ paper: classes.drawerPaper }}
-            ModalProps={{ keepMounted: true }}
-          >
-            <div>
-              <div className={classes.toolbar} />
-              <Divider />
-              <List>
-                {Object.entries(users).map(([id, info]) => (
-                  <ListItem button key={id}>
-                    <ListItemIcon><PersonIcon style={{
-                      backgroundColor: '#3f51b5',
-                      borderRadius: '50%',
-                      width: 30,
-                      height: 30,
-                      color: 'white',
-                    }} /></ListItemIcon>
-                    <ListItemText primary={info.name} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            <div>
-              <div className={classes.toolbar} />
-              <Divider />
-              <List>
-                {Object.entries(users).map(([id, info]) => (
-                  <ListItem button key={id}>
-                    <ListItemIcon><PersonIcon style={{
-                      backgroundColor: '#3f51b5',
-                      borderRadius: '50%',
-                      width: 30,
-                      height: 30,
-                      color: 'white',
-                    }} /></ListItemIcon>
-                    <ListItemText primary={info.name} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Drawer>
-        </Hidden>
-      </nav>
-      <div className="App">
-        <div className="editor">
-          <Editor
-            onMount={(editor) => setEditor(editor)}
-          />
-        </div>
-      </div>
+    <div>
+      {screen}
     </div>
   );
 }
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-    },
-    drawer: {
-      [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-    },
-    appBar: {
-      [theme.breakpoints.up('sm')]: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-      },
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
-      },
-    },
-    // necessary for content to be below app bar
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
-  }),
-);
